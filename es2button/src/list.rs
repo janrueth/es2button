624df7ac087cd7e3ca5ca79d7e811c68e5fca8d5
Jan Rüth <jan@djiehmail.com>
2025-02-24 19:09:28 +0100

    Switch from libusb to rusb
    Libusb crate doesn't build on recent cargo versions and seems to be abandoned.
    rusb is a maintained fork.
    This commit switches to it.

--- es2button/src/list.rs
+++ es2button/src/list.rs
@@ -1,5 +1,7 @@
 use std::time::Duration;
 
+use rusb::UsbContext;
+
 use crate::types::{self, UsbDevice, UsbIdentifier};
 
 const EPSON_USB_VID: u16 = 0x04b8;
@@ -7,14 +9,14 @@ const EPSON_USB_VID: u16 = 0x04b8;
 pub(crate) fn epson_scanners() -> Vec<UsbDevice> {
     let mut devices = Vec::<types::UsbDevice>::new();
 
-    let ctx = libusb::Context::new().unwrap();
+    let ctx = rusb::Context::new().unwrap();
     for device in ctx.devices().unwrap().iter() {
         let descriptor = device.device_descriptor().unwrap();
 
         if descriptor.vendor_id() != EPSON_USB_VID {
             continue;
         }
-        let mut handle = device.open().unwrap();
+        let handle = device.open().unwrap();
         handle.reset().unwrap();
 
         let langs = handle.read_languages(Duration::from_secs(1)).unwrap();
